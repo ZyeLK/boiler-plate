1ccdd97c064405f2b2d6cc3c67ef86af54066bed
Taewon <hengunkaduk@naver.com>
2020-07-26 10:37:07 +0900

    비밀번호 암호화

--- index.js
+++ index.js
@@ -13,16 +13,16 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true})); // application/x-www-form-urlencoded 꼴 데이터 분석해서 가져올 수 있게
 app.use(bodyParser.json()); // application/json 꼴 데이터 분석해서 가져올 수 있게
 
-
 const { User } = require("./models//User");
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+// register route
 app.post('/register', (req, res) => { // postman에서 http://localhost:3000/register로 POST해야 함
     //회원가입 시 필요한 정보들을 클라이언트에서 가져오면
-    //그것들을 데이터베이스에 넣어준다
-    
     const user = new User(req.body)
+
+    //정보를 데이터베이스에 넣어준다
     user.save((err, userInfo) => {
         if(err) return res.json({ success: false, err}) // 에러 뜰 경우 false 출력
         return res.status(200).json({ success: true}) // status(200): 성공이란 뜻
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10; // salt가 몇 글자인지 나타냄
 
 const userSchema = mongoose.Schema({
     name: {
@@ -28,6 +30,27 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {
         type: Number // 유효기간 (Expiration)
+    }
+})
+
+// 유저 모델에 유저 정보 저장하기 전에 할 것 (새로운 정보를 저장할 때마다 이 부분 실행됨)
+userSchema.pre('save', function(next){
+    var user = this; // 새로 받은 user을 가리키는 변수
+
+    // 비밀번호 업데이트 시
+    if(user.isModified('password')){
+        // 비밀번호 암호화
+        bcrypt.genSalt(saltRounds, function(err, salt) { // 10자리 salt 이용해 비밀번호 암호화
+            if(err) return next(err) // 에러 발생 시 바로 에러로 보내줌(index.js의 user.save 부분으로)
+            
+            bcrypt.hash(user.password, salt, function(err, hash) { // plain text로 된 비밀번호와 salt 이용. hash가 암호화된 비밀번호
+                if(err) return next(err)
+                user.password = hash // 암호화된 비밀번호로 교체해 저장
+                next() // 원래로 돌아감
+            });
+        });
+    }else{
+        next() // next를 반드시 해 줘야 원래로 돌아감
     }
 })
 
